d63c15eaa63c0d3ecdfc69dfde9e1c9d0b07e3d6
laurilautervikk <lauri.lauter@vikk.ee>
2022-05-05 22:08:51 +0300

    Initial commit again

---
+++ server/index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const mongoose = require("mongoose"); // new
+const routes = require("./routes");
+const port = process.env.PORT || 5000;
+
+// Connect to MongoDB database
+mongoose
+  .connect("mongodb://localhost:27017/shop_db", { useNewUrlParser: true })
+  .then(() => {
+    const app = express();
+    app.use(express.json());
+    app.use("/api", routes);
+
+    app.listen(port, () => {
+      console.log(`Server running at http://localhost:${port}/`);
+    });
+  });
---
+++ server/models/Item.js
@@ -0,0 +1,10 @@
+const mongoose = require("mongoose");
+
+const schema = mongoose.Schema({
+  name: { type: String },
+  amount: Number,
+  author: String,
+  active: Boolean,
+});
+
+module.exports = mongoose.model("Item", schema);
