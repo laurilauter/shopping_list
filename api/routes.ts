3aeefa3c0c0f1309c4bd40ffd7f843df536e5962
Lauri Lauter <lauri.lauter@gmail.com>
2022-07-21 23:27:42 +0300

    cors removed

--- api/routes.ts
+++ api/routes.ts
@@ -1,11 +1,7 @@
 const express = require("express");
-const cors = require("cors");
 const Item = require("./models/Item");
 const router = express.Router();
 const path = require("path");
-
-//use cors
-router.use(cors());
 
 // Serve static files from the frontend app
 router.use(express.static(path.join(__dirname, "../dist/public")));
