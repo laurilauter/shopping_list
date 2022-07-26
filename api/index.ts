7e8f4d190c57b66a12389c0b10a5861eed6659dd
Lauri Lauter <lauri.lauter@gmail.com>
2022-07-26 22:33:09 +0300

    simplified10

--- api/index.ts
+++ api/index.ts
@@ -14,15 +14,16 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true }).then(() => {
   app.use("/api", routes);
 
   /* serve your front (stored in the public folder) */
-  //app.use("/", express.static("./dist/public"));
+  app.use("/", express.static("./dist/public"));
 
-  app.use(express.static(path.join(__dirname, "dist", "public")));
+  //app.use(express.static(path.join(__dirname, "dist", "public")));
   // required to serve SPA on heroku production without routing problems; it will skip only 'api' calls
-  if (process.env.NODE_ENV === "production") {
-    app.get(/^((?!(api)).)*$/, (req: any, res: any) => {
-      res.sendFile(path.join(__dirname, "dist/public", "index.html"));
-    });
-  }
+  //if (process.env.NODE_ENV === "production") {
+  app.get(/^((?!(api)).)*$/, (req: any, res: any) => {
+    res.sendFile(path.join(__dirname, "dist/public/index.html"));
+    console.log("path: ", path.join(__dirname, "dist/public/index.html"));
+  });
+  //}
 
   app.listen(port, () => {
     console.log(`Server running at port:${port}`);
