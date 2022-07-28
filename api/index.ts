9573bf66b1ddfd24faff30b27b9a294255e6d045
Lauri Lauter <lauri.lauter@gmail.com>
2022-07-28 23:32:20 +0300

    paths weird 5

--- api/index.ts
+++ api/index.ts
@@ -15,15 +15,15 @@ mongoose.connect(MONGO_URI, { useNewUrlParser: true }).then(() => {
   app.use("/api", routes);
 
   /* serve your front (stored in the public folder) */
-  app.use("/", express.static("./dist/public"));
+  app.use("/", express.static(path.join(__dirname, "../public")));
 
   //app.use(express.static(path.join(__dirname, "dist", "public")));
   // required to serve SPA on heroku production without routing problems; it will skip only 'api' calls
   //if (process.env.NODE_ENV === "production") {
+
   app.get(/^((?!(api)).)*$/, (req: any, res: any) => {
-    res.sendFile(path.join("./app/dist/public/index.html")); //Error: ENOENT: no such file or directory, stat '/dist/public/index.html'
-    console.log("BrokenPath ", path.join("./app/dist/public/index.html"));
-    // res.sendFile(path.join(__dirname + "/../public/index.html"));
+    res.sendFile(path.join(__dirname, "../public/index.html"));
+    console.log("BrokenPath ", path.join(__dirname, "../public/index.html"));
   });
   //}
 
